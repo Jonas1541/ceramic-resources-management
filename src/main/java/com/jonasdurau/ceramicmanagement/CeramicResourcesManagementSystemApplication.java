4ff441b34d43a0ddd4e98b45d8137e38735bb2a0
Jonas Durau <jonasdurau@gmail.com>
2024-12-06 09:37:06 -0300

    first commit

---
+++ src/main/java/com/jonasdurau/ceramicmanagement/CeramicResourcesManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jonasdurau.ceramicmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeramicResourcesManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeramicResourcesManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonasdurau/ceramicmanagement/CeramicResourcesManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonasdurau.ceramicmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeramicResourcesManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
