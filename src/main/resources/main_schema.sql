fe577b69ff146297b5791bfb0691e6c70bbbb775
Jonas Durau <jonasdurau@gmail.com>
2025-02-25 19:54:08 -0300

    refactor(main_schema.sql): update main_schema script with correct tb_company fields

--- src/main/resources/main_schema.sql
+++ src/main/resources/main_schema.sql
@@ -1,13 +1,13 @@
-create table tb_company (
-    id bigint not null auto_increment,
-    name varchar(255) not null,
-    email varchar(255) not null,
-    cnpj varchar(14) not null,
-    database_name varchar(255) not null,
-    primary key (id)
-) engine=InnoDB;
-
-alter table tb_company 
-    add constraint UKruse2bynlu533jtqlc6r4g4xh unique (cnpj),
-    add constraint UKgq2acy6se5ohgkps24kpeuxgk unique (database_name),
-    add constraint UKa895hjqwnuln8njdho9nxsqnd unique (email);
+CREATE TABLE tb_company (
+  id bigint NOT NULL AUTO_INCREMENT,
+  cnpj varchar(255) DEFAULT NULL UNIQUE,
+  created_at datetime DEFAULT CURRENT_TIMESTAMP,
+  database_port int DEFAULT NULL,
+  database_name varchar(255) DEFAULT NULL,
+  database_url varchar(255) DEFAULT NULL,
+  email varchar(255) DEFAULT NULL UNIQUE,
+  name varchar(255) DEFAULT NULL,
+  password varchar(255) DEFAULT NULL,
+  updated_at datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB
