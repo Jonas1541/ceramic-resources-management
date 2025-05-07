afc5d298ae5a991211725b4810d19c7e49a9aea1
Jonas <jonasdurau@gmail.com>
2025-05-07 11:16:57 -0300

    fix(cors): add PATCH method to allowed CORS methods

--- src/main/java/com/jonasdurau/ceramicmanagement/config/SecurityConfigurations.java
+++ src/main/java/com/jonasdurau/ceramicmanagement/config/SecurityConfigurations.java
@@ -45,7 +45,7 @@ public class SecurityConfigurations {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
 
